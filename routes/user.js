d712bed2c78c826372fdfcc8d535f93f306b0cc9
SHUBHAM AGARWAL <shubham.agarwal2019vit@gmail.com>
2022-06-02 11:03:21 +0530

    Crud on database complete

--- routes/user.js
+++ routes/user.js
@@ -4,7 +4,8 @@ const passport=require('passport');
 
 const userController = require('../controllers/user_controllers');
 
-router.get('/profile', passport.checkAuthentication, userController.profile);
+router.get('/profile/:id', passport.checkAuthentication, userController.profile);
+router.post('/update/:id', passport.checkAuthentication, userController.update);
 router.get('/signIn', userController.signIn);
 router.get('/signUp', userController.signUp);
 
